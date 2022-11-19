07bb2130b74f0c8cce9ce928414860dc01d76b89
Swadip <roysoumo1607@gmail.com>
2022-11-20 01:51:41 +0600

    solve logical mistake inside public --> scripts --> profilePicsUpload.js

--- public/scripts/profilePicsUpload.js
+++ public/scripts/profilePicsUpload.js
@@ -12,7 +12,7 @@ profilePicsFile.addEventListener('change', function () {
       image.src = event.target.result
       image.onload = function () {
         const uploadedFileSize = profilePicsFile.files[0].size / 1024
-        const maxFileSize = 1024 * 2
+        const maxFileSize = 1024 * 1024 * 2
         let height = this.height;
         let width = this.width;
         let _this = reader
